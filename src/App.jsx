e0bc3fe8a4866bbc6376b70e9791a5511ec7af42
stanley_Huang <s703702002@gmail.com>
2019-09-08 00:06:28 +0800

    dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,98 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import './App.css';
-import Select from './Select'
-import { Row, Label, FormField } from './Grid'
-import { formatDate } from './utils'
-import { Desc, Asc, Exchange } from './Icons'
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import "./App.css";
+import Select from "./Select";
+import { Row, Label, FormField } from "./Grid";
+import { formatDate } from "./utils";
+import { Desc, Asc } from "./Icons";
 
 // API spec: https://ptx.transportdata.tw/MOTC?t=Rail&v=2#!/THSR/THSRApi_DailyTimetable
-const API_BASE = 'https://ptx.transportdata.tw/MOTC'
-const API_TODAY = '/v2/Rail/THSR/DailyTimetable/Today' // GET 取得當天所有車次的時刻表資料
-
+const API_BASE = "https://ptx.transportdata.tw/MOTC";
+// const API_TODAY = '/v2/Rail/THSR/DailyTimetable/Today' // GET 取得當天所有車次的時刻表資料
 
 const fetchOptions = {
-  method: 'GET',
-  mode: 'cors',
+  method: "GET",
+  mode: "cors",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json"
   }
-}
+};
 
 function searchTrain(OriginStationID, DestinationStationID, TrainDate) {
   // GET 取得指定[日期],[起迄站間]之時刻表資料
-  const API_TRAIN = `/v2/Rail/THSR/DailyTimetable/OD/${OriginStationID}/to/${DestinationStationID}/${TrainDate}`
-  return fetch(API_BASE + API_TRAIN, fetchOptions)
+  const API_TRAIN = `/v2/Rail/THSR/DailyTimetable/OD/${OriginStationID}/to/${DestinationStationID}/${TrainDate}`;
+  return fetch(API_BASE + API_TRAIN, fetchOptions);
 }
 
 function searchPriceByStation(OriginStationID, DestinationStationID) {
   // GET 取得指定[起訖站間]之票價資料
-  const API_PRICE = `/v2/Rail/THSR/ODFare/${OriginStationID}/to/${DestinationStationID}`
-  return fetch(API_BASE + API_PRICE, fetchOptions)
+  const API_PRICE = `/v2/Rail/THSR/ODFare/${OriginStationID}/to/${DestinationStationID}`;
+  return fetch(API_BASE + API_PRICE, fetchOptions);
 }
 
 function getODFARE() {
-  const API_ODFARE = '/v2/Rail/THSR/ODFare' // GET 取得票價資料
-  return fetch(API_BASE + API_ODFARE, fetchOptions)
+  const API_ODFARE = "/v2/Rail/THSR/ODFare"; // GET 取得票價資料
+  return fetch(API_BASE + API_ODFARE, fetchOptions);
 }
 
 function App() {
-  const [updateTime, setUpdateTime] = useState('')
-  const [stationOptions, setStationOptions] = useState([])
-  const [date, setDate] = useState(formatDate(Date.now()))
-  const [sortByDeparture, setDeaprtureState] = useState('desc')
-  const [sortByArrival, setArrivalState] = useState('desc')
-  const [departure, setDeparture] = useState('1000') // 台北
-  const [arrival, setArrival] = useState('1070') // 左營
-  const [resultList, setResultList] = useState([]) 
+  const [updateTime, setUpdateTime] = useState("");
+  const [stationOptions, setStationOptions] = useState([]);
+  const [date, setDate] = useState(formatDate(Date.now()));
+  const [sortByDeparture, setDeaprtureState] = useState("desc");
+  const [sortByArrival, setArrivalState] = useState("desc");
+  const [departure, setDeparture] = useState("1000"); // 台北
+  const [arrival, setArrival] = useState("1070"); // 左營
+  const departureRef = useRef();
+  const arrivalRef = useRef();
+  const [resultList, setResultList] = useState([]);
+  const [prizeList, setPrizeList] = useState([]);
 
   const changeDeparture = useCallback(
-    (e) => setDeparture(e.currentTarget.value),
-    [setDeparture],
-  )
-  const changeArrival = useCallback(
-    (e) => setArrival(e.currentTarget.value),
-    [setArrival],
-  )
+    e => setDeparture(e.currentTarget.value),
+    [setDeparture]
+  );
+
+  const changeArrival = useCallback(e => setArrival(e.currentTarget.value), [
+    setArrival
+  ]);
+
+  const DepArrChange = useCallback(() => {
+    const currentD = departure;
+    const currentA = arrival;
+    setDeparture(currentA);
+    setArrival(currentD);
+  }, [departure, arrival]);
 
   useEffect(() => {
     getODFARE()
       .then(res => res.json())
       .then(data => {
-        const stations = {}
+        const stations = {};
         data.forEach(element => {
-          const stationsId = element.OriginStationID
+          const stationsId = element.OriginStationID;
           if (!stations[stationsId]) {
-            stations[stationsId] = element.OriginStationName
+            stations[stationsId] = element.OriginStationName;
           }
         });
         setStationOptions(
-          Object.entries(stations)
-            .map(e => ({
-              value: e[0],
-              ...e[1]
-            }))
-        )
-        setUpdateTime(data[0].UpdateTime)
+          Object.entries(stations).map(e => ({
+            value: e[0],
+            ...e[1]
+          }))
+        );
+        setUpdateTime(data[0].UpdateTime);
       })
       .catch(err => {
-        console.log('error', err)
-      })
-  }, [])
-
+        console.log("error", err);
+      });
+  }, []);
+  console.log("prizeList", prizeList);
   return (
     <div className="App">
       <header>
         <h1>高鐵查詢系統</h1>
       </header>
-      <div className='search_panel container'>
+      <div className="search_panel container">
         <Row>
           <Label htmlFor="trip-start">日期:</Label>
           <FormField>
@@ -93,7 +101,7 @@ function App() {
               type="date"
               id="trip-start"
               value={date}
-              onChange={(e) => setDate(e.currentTarget.value)}
+              onChange={e => setDate(e.currentTarget.value)}
             />
           </FormField>
         </Row>
@@ -107,27 +115,33 @@ function App() {
               options={stationOptions}
               value={departure}
               onChange={changeDeparture}
+              ref={departureRef}
             />
           </FormField>
         </Row>
         <div className="row justify-content-center form-group">
-          <button className="col-2">
-            <Exchange />
+          <button
+            onClick={DepArrChange}
+            type="button"
+            className="btn btn-outline-primary"
+          >
+            起迄站交換
           </button>
         </div>
         <Row>
           <Label htmlFor="DestinationStation">迄站:</Label>
           <FormField>
-            <Select 
-              className="form-control" 
+            <Select
+              className="form-control"
               name="DestinationStation"
               id="DestinationStation"
               options={stationOptions}
               value={arrival}
               onChange={changeArrival}
+              ref={arrivalRef}
             />
           </FormField>
-        </Row>  
+        </Row>
         <button
           type="button"
           className="btn btn-primary btn-lg btn-block"
@@ -135,15 +149,13 @@ function App() {
             searchTrain(departure, arrival, date)
               .then(res => res.json())
               .then(data => {
-                console.log('data', data)
+                console.log("data", data);
               })
-              .catch(err => console.log(err))
+              .catch(err => console.log(err));
             searchPriceByStation(departure, arrival)
               .then(res => res.json())
-              .then(data => {
-                console.log('dataa', data)
-              })
-              .catch(err => console.log(err))
+              .then(data => setPrizeList(data[0].Fares))
+              .catch(err => console.log(err));
           }}
         >
           查詢
@@ -155,37 +167,61 @@ function App() {
             <tr>
               <td>車次</td>
               <td>起迄站</td>
-              <td className="pointer" onClick={() => setDeaprtureState(
-                sortByDeparture === 'desc' ? 'asc' : 'desc'
-              )}>
+              <td
+                className="pointer"
+                onClick={() =>
+                  setDeaprtureState(sortByDeparture === "desc" ? "asc" : "desc")
+                }
+              >
                 發車
-                { sortByDeparture === 'desc' ? <Desc /> : <Asc />}
+                {sortByDeparture === "desc" ? <Desc /> : <Asc />}
               </td>
-              <td className="pointer" onClick={() => setArrivalState(
-                sortByArrival === 'desc' ? 'asc' : 'desc'
-              )}>
+              <td
+                className="pointer"
+                onClick={() =>
+                  setArrivalState(sortByArrival === "desc" ? "asc" : "desc")
+                }
+              >
                 到達
-                { sortByArrival === 'desc' ? <Desc /> : <Asc />}
+                {sortByArrival === "desc" ? <Desc /> : <Asc />}
               </td>
               <td>總時程</td>
             </tr>
           </thead>
-          <tbody>
-            
-          </tbody>
+          <tbody></tbody>
         </table>
-        {
-          !resultList.length && <p>請查詢</p>
-        }
+        {!resultList.length && <p>尚無資料</p>}
         <div>
           <h3>票價資訊</h3>
+          {prizeList.length > 0 ? (
+            <table className="prize_table">
+              <thead>
+                <tr>
+                  <th colSpan="3">
+                    {departureRef.current.selectedOptions[0].textContent}
+                    <span className="ml-2 mr-2">&rarr;</span>
+                    {arrivalRef.current.selectedOptions[0].textContent}
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  {prizeList.map(v => (
+                    <td key={v.Price}>
+                      {v.TicketType}:<strong>&#36;{v.Price}</strong>
+                    </td>
+                  ))}
+                </tr>
+              </tbody>
+            </table>
+          ) : null}
         </div>
       </div>
       <footer>
         <p>更新時間: {updateTime}</p>
       </footer>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/Select.jsx
+++ src/Select.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const Select = React.memo(({
-  options,
-  ...props
-}) => {
-  return (
-    <select {...props}>
-      {
-        options.length > 0 ?
+const Select = React.memo(
+  React.forwardRef(({ options, ...props }, ref) => {
+    console.log("render");
+    return (
+      <select {...props} ref={ref}>
+        {options.length > 0 ? (
           options.map(e => (
-            <option key={e.value} value={e.value}>{e.Zh_tw + e.En}</option>
-          )) : <option value="">請選擇</option>
-      }
-    </select>
-  )
-})
+            <option key={e.value} value={e.value}>
+              {e.Zh_tw + e.En}
+            </option>
+          ))
+        ) : (
+          <option value="">請選擇</option>
+        )}
+      </select>
+    );
+  })
+);
 
-export default Select
+export default Select;
