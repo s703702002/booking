70a6b15f383d94a816452a9a9c2f442ff1dede82
zhuang01 <zhaoyuan.huang@oath.com>
2019-09-17 15:18:21 +0800

    Updates

---
+++ precache-manifest.2715c45868286719efca4c67896cdbaf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d30e7e6e761fc7b1199b3c4f6aa5d5",
+    "url": "/booking/index.html"
+  },
+  {
+    "revision": "ad5116923b4f71610391",
+    "url": "/booking/static/css/main.7421dfa3.chunk.css"
+  },
+  {
+    "revision": "9e0cb831c134c4b87416",
+    "url": "/booking/static/js/2.17210fbd.chunk.js"
+  },
+  {
+    "revision": "ad5116923b4f71610391",
+    "url": "/booking/static/js/main.6e3866ba.chunk.js"
+  },
+  {
+    "revision": "a695529b8a7c0e54eb19",
+    "url": "/booking/static/js/runtime~main.2a064130.js"
+  },
+  {
+    "revision": "d8be3f88ebe0fa31d562289dba77a90c",
+    "url": "/booking/static/media/asc.d8be3f88.svg"
+  },
+  {
+    "revision": "f06acfc6598433894a74effa7652e3fa",
+    "url": "/booking/static/media/desc.f06acfc6.svg"
+  },
+  {
+    "revision": "c7b5a736f5dc77e1294b5ce362cb8c5c",
+    "url": "/booking/static/media/exchange.c7b5a736.svg"
+  }
+]);
