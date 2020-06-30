9d1798eee39f385e4bf5157a49065b8cde258901
Stanley Huang <stanley.huang@gogoro.com>
2020-06-30 14:52:46 +0800

    Add jsconfig.json for vscode reslove absolute path import

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,11 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true,
-    jest: true,
   },
   extends: [
     "react-app",
     "plugin:prettier/recommended",
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
