9393276697f023cd72ffac77de724d622b8b0cac
Stanley Huang <stanley.huang@gogoro.com>
2020-07-01 11:37:47 +0800

    Updates

---
+++ precache-manifest.34ff605255b0e3c19fc6187eca3a1780.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79eb0ed712b7253e78d638c4e3df7e05",
+    "url": "/booking/index.html"
+  },
+  {
+    "revision": "9b2689511e36da226006",
+    "url": "/booking/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "ee0225a271decf59c8e3",
+    "url": "/booking/static/js/2.8cfa5366.chunk.js"
+  },
+  {
+    "revision": "32d271d94f6cae95556f7eca288c4605",
+    "url": "/booking/static/js/2.8cfa5366.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b2689511e36da226006",
+    "url": "/booking/static/js/main.70d98593.chunk.js"
+  },
+  {
+    "revision": "3757a2d75200f99586ee",
+    "url": "/booking/static/js/runtime-main.6af8a0d0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/booking/precache-manifest.34f11fc3d16f47308e37b38a12076a67.js"
+  "/booking/precache-manifest.34ff605255b0e3c19fc6187eca3a1780.js"
 );
 
 self.addEventListener('message', (event) => {
